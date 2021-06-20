8e98882807bddbac1fb41ba474368e04524f43b1
송유현 <doong3373@gmail.com>
2021-06-20 18:35:47 +0900

    make entity and service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -8,4 +8,21 @@ export class MoviesService {
     getAll():Movie[] {
         return this.movies;
     }
+
+    getOne(id:string):Movie{
+        // string으로 받아온 id값을 int로 형변환
+        return this.movies.find(movie => movie.id === +id);
+    }
+
+    deleteOne(id:string):boolean{
+        this.movies.filter(movie => movie.id !== +id);
+        return true;
+    }
+
+    create(movieData){
+        this.movies.push({
+            id: this.movies.length + 1,
+            ...movieData,
+        })
+    }
 }
