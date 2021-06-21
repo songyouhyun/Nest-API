fd13eadfb59204f8e73a92bd6ae78ecbbea3dd78
송유현 <doong3373@gmail.com>
2021-06-21 23:52:39 +0900

    ADD DTO and Validation(first)

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movies.dto';
 import { Movie } from './entity/movies.entity';
 import { MoviesService } from './movies.service';
 
@@ -11,22 +12,23 @@ export class MoviesController {
     } 
     
     @Get(":id")
-    getOne(@Param('id') movieId: string):Movie {
+    getOne(@Param('id') movieId: number):Movie {
+        console.log(typeof movieId);
         return this.moviesService.getOne(movieId);
     }
     
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch(':id')
-    patch(@Param('id') movieId: string, @Body() updateData){
+    patch(@Param('id') movieId: number, @Body() updateData){
     return this.moviesService.update(movieId, updateData);
     }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movies.dto';
 import { Movie } from './entity/movies.entity';
 
 @Injectable()
@@ -9,32 +10,31 @@ export class MoviesService {
         return this.movies;
     }
 
-    getOne(id:string):Movie{
+    getOne(id:number):Movie{
         // string으로 받아온 id값을 int로 형변환
-        const movie =  this.movies.find(movie => movie.id === +id);
+        const movie =  this.movies.find(movie => movie.id === id);
         if(!movie){
             throw new NotFoundException(`Movie with ID ${id} is not found`);
         }
         return movie;
     }
 
-    deleteOne(id:string){
+    deleteOne(id:number){
         this.getOne(id);
-        this.movies = this.movies.filter(movie => movie.id !== +id);
+        this.movies = this.movies.filter(movie => movie.id !== id);
     }
 
-    create(movieData){
+    create(movieData: CreateMovieDto){
         this.movies.push({
             id: this.movies.length + 1,
             ...movieData,
         })
     }
     
-    update(id:string, updateData){
+    update(id:number, updateData){
         // id가 1인 movie를 가져온다 -> 그 movie를 지운다 -> 과거의 데이터에 새로운 데이터를 더해서 새로운 movie를 만든다
         const movie = this.getOne(id);
         this.deleteOne(id);
         this.movies.push({ ...movie, ...updateData });
-
     }
 }
