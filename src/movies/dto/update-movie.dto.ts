188ae0b059160cfcf6b2bbeb69c0e89fc70ec03d
송유현 <doong3373@gmail.com>
2021-06-22 23:52:19 +0900

    complete DTO

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from "@nestjs/mapped-types"
+import { CreateMovieDto } from "./create-movies.dto";
+
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+    
+}
